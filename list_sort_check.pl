4a935d2d0af4ce941875ee9e561e839379cfb210
Rana-Saleh44 <ranamhsaleh@gmail.com>
2024-03-15 09:54:54 +0200

    Update list_sort_check.pl

--- list_sort_check.pl
+++ list_sort_check.pl
@@ -1,3 +1,3 @@
 is_sorted([]).
 is_sorted([_]). %I have only one element in the list
-is_sorted([X,Y|T]):- X=<Y, is_sorted([Y|T]).
+is_sorted([X,Y|T]):- X=<Y, is_sorted([Y|T]).%I compare each 1st 2 elements and so on
