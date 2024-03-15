ad7f1d2c9b859d3a621f4b3d3889c4caa55d12e9
Rana-Saleh44 <ranamhsaleh@gmail.com>
2024-03-15 06:30:55 +0200

    Create Anonymous_variable.pl

---
+++ Anonymous_variable.pl
@@ -0,0 +1,10 @@
+hates(mary,jane).
+hates(ain,nobita).
+hates(mom,neighbor).
+hates(dawson,oswald).
+hates(david,bowling).
+hates(harry,oswald).
+hates(winnie,trot).
+
+% Anonymous variable we use _ to make the answer is true or false I
+% don't care the name or the exact value
