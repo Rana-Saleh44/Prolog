c45cd087b73a3de264707da1434cf30d9b7b813b
Rana Saleh <137465053+Rana-Saleh44@users.noreply.github.com>
2024-03-15 15:04:23 +0200

    Create Backtracking.pl

---
+++ Backtracking.pl
@@ -0,0 +1,10 @@
+% in prolog the ; is different because it trigger the solution not like the other programming language once it find the solution it will stop
+possible_pair(X,Y):- boy(X),girl(Y).
+%tree like structure 1st the left part will be traversed
+boy(john).
+boy(harry).
+girl(alisa).
+girl(maya).
+
+is_integer(0).
+is_integer(X):- is_integer(Y), X is Y+1.
