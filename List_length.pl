f018f6e2852dc27e729b6d8f14b6e8ddcc39bd3e
Rana-Saleh44 <ranamhsaleh@gmail.com>
2024-03-15 18:25:49 +0200

    created lab recap & modified list length

--- List_length.pl
+++ List_length.pl
@@ -3,3 +3,4 @@ size([_|T],N):- size(T,N1), N is N1+1.
 %this is a recursive call so the stack will be made
 % singleton variable means that the variable is used once in the entire
 % program
+parent(saleh,rana).
