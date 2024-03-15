324b090e0ed433af5cd424a2aef84e4d87c61f90
Rana-Saleh44 <ranamhsaleh@gmail.com>
2024-03-15 02:03:17 +0200

    Update operators.pl

--- operators.pl
+++ operators.pl
@@ -1,7 +1,17 @@
-father(ahmed, ali).
-father(ali, rana).
-father(ahmed, mona).
-
-run :-
-    father(X, Y),
-    write(X), write(' is the father of '), write(Y), nl.
+father(ahmed,ali).
+father(ali,rana).
+father(ahmed,amr).
+run:-father(X,Y),write(X," is the father of ",Y),ln,fail.
+%the operator are fail, not, cutoff
+%fail is for backtracking
+%it should print:
+%ahmed is the father of ali
+%ali is the father of rana
+%ahmed is the father of amr
+%no
+%if I want to print yes not no
+%run.    !! again
+%not is for the opposite of the answer
+%ex: test.
+%not(test)
+%cutoff ! it prevent the backtrack
