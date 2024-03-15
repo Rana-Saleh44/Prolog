31524a43a40cf754a614f953fc286e011dbc7196
Rana-Saleh44 <ranamhsaleh@gmail.com>
2024-03-15 07:13:56 +0200

    Create Equality_and_unification.pl

---
+++ Equality_and_unification.pl
@@ -0,0 +1,10 @@
+success(rana,saleh).
+%x=y
+%y=10 then x will = 10 automatically
+a(b,c(d,e)).
+a(X,c(Y,e)).
+%then X will = b and Y will = d automatically
+%what ever is inside a '' is an atom
+%'student'=student.  this is true
+%f(X,X)=f(a,b).   false cus same var with different valuse
+%f(_,_)=f(a,b).   true :)
