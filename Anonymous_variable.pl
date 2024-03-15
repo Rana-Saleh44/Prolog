8723e42d9d5d61c89a90b9194685495ca1ebbf47
Rana-Saleh44 <ranamhsaleh@gmail.com>
2024-03-15 06:43:17 +0200

    Update Anonymous_variable.pl

--- Anonymous_variable.pl
+++ Anonymous_variable.pl
@@ -4,7 +4,8 @@ hates(mom,neighbor).
 hates(dawson,oswald).
 hates(david,bowling).
 hates(harry,oswald).
-hates(winnie,trot).
+hates(winnie,person(rana,saleh)).
 
 % Anonymous variable we use _ to make the answer is true or false I
 % don't care the name or the exact value
+% _ _ THEY ARE DIFFERENT FROM EACH OTHER
