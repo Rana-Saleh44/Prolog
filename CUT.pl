6d02db2a7fcdccaa8e65de4cf9848712f6c1d491
Rana Saleh <137465053+Rana-Saleh44@users.noreply.github.com>
2024-03-15 15:30:26 +0200

    Create CUT.pl

---
+++ CUT.pl
@@ -0,0 +1,6 @@
+% CUT control backtracking (stop the backtracking in certain cases) benefits(it tells the program that I find the right path'track')and also we don't want to have multiple results (particular) through the backtracking
+% which saves alot of processing power,time, RAM since it is decreasing the domain
+% mark the point and make it as check point and !!! I can't go back from the check point then I report false 
+%I can have one CUT or more
+% once we cross the (aka the previous command before the ! is successed ) ! we can't backtrack in the previous but I can backtrack in the following 
+
